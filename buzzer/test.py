d58aa6bed5e909d1b8533e8dd6a4314c87d5fb33
Michal Vala <mvala@redhat.com>
2019-01-21 22:02:17 +0100

    buzzer test

---
+++ buzzer/test.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+buzzer_pin = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buzzer_pin, GPIO.IN)
+GPIO.setup(buzzer_pin, GPIO.OUT)
+
+frequency = 440
+period = 1.0 / frequency
+delayValue = period / 2
+
+print("sleep", delayValue)
+
+try:
+    while True:
+        GPIO.output(buzzer_pin, True)
+        time.sleep(delayValue)
+        GPIO.output(buzzer_pin, False)
+        time.sleep(delayValue)
+except KeyboardInterrupt:
+    print(" bye")
+finally:
+    GPIO.cleanup()
